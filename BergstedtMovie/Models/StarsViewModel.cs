4ebaf03d70e8ce0e0e6f2bb97de7aa54e0d7f3d7
TB <tb@example.com>
2019-05-16 15:55:05 +0200

    Added stars component

---
+++ BergstedtMovie/Models/StarsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BergstedtMovie.Models
+{
+    public class StarsViewModel
+    {
+        public int Stars { get; set; }
+        public bool IsHalfStar { get; set; }
+    }
+}
---
+++ BergstedtMovie/ViewComponents/StarViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using BergstedtMovie.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BergstedtMovie.ViewComponents
+{
+    public class StarViewComponent : ViewComponent
+    {
+        private readonly BergstedtMovieContext _context;
+
+        public StarViewComponent(BergstedtMovieContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int movieId)
+        {
+            var movie = await _context.Movie.FindAsync(movieId);
+            var doubleRating = (int)Math.Round(movie.Rating * 2);
+            var model = new StarsViewModel
+            {
+                Stars = doubleRating / 2,
+                IsHalfStar = doubleRating % 2 == 1
+            };
+            return View(model);
+        }
+    }
+}
